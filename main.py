7b02d317fcab070d8528966394333e91d3ed342c
Zordenones <133235436+Zordenones@users.noreply.github.com>
2023-08-10 12:06:31 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#EXERCICIO 2
+#import datetime
+#Hoje = datetime.date.today()
+#AnoAtual = Hoje.year
+#AnoDeNascimento = int(input("qual o ano de nascimento? "))
+#idade = AnoAtual - AnoDeNascimento
+#print("Vc fez:", idade)
+
+#EXERCICIO 3
+#Salario = float(input("Qual o seu salario? "))
+#SalarioMin = float(1320.00)
+
+#QNTSalários = Salario / SalarioMin
+#print("Voce recebe:", QNTSalários)
+
+#EXERCICIO 4
+D = int(input("Insira o diameatro:"))
+R = D / 2
+PI = 3.141592653
+C = R**2*PI
+print(C)
